1b00d1243b23f01b437b1c0721fda8bab14d8cdf
Justin Mei <imjustinmei@gmail.com>
2022-04-23 14:16:54 -0400

    fix formatting

--- maze.py
+++ maze.py
@@ -1,44 +1,43 @@
 import random
-import matplotlib.pyplot as plt
 
 def neighbors(cell):
-  return [[cell[0] + x, cell[1]] for x in [-1, 1]] + [[cell[0], cell[1] + x] for x in [-1, 1]]
+    return [[cell[0] + x, cell[1]] for x in [-1, 1]] + [[cell[0], cell[1] + x] for x in [-1, 1]]
 
 def generateMaze(size, start=1):
-  #randomized prim's algorithm
-  wlist = []
-  maze = [['u' for y in range(size + 2)] for x in range(size + 2)]
-  maze[start][start] = 'c'
+    # randomized prim's algorithm
+    wlist = []
+    maze = [['u' for y in range(size + 2)] for x in range(size + 2)]
+    maze[start][start] = 'c'
 
-  for x in neighbors([start, start]):
-    wlist.append([x[0], x[1]])
+    for x in neighbors([start, start]):
+        wlist.append([x[0], x[1]])
 
-  while wlist:
-    randWall = random.choice(wlist)
-    if 0 < randWall[0] < size + 1 and 0 < randWall[1] < size + 1:
-      if [maze[x[0]][x[1]] == 'c' for x in neighbors([randWall[0], randWall[1]])].count(True) < 2: 
-        vertical = (maze[randWall[0] + 1][randWall[1]] == 'c') != (maze[randWall[0] - 1][randWall[1]] == 'c') and (maze[randWall[0] + 1][randWall[1]] == 'u') != (maze[randWall[0] - 1][randWall[1]] == 'u')
-        horizontal = (maze[randWall[0]][randWall[1]+1] == 'c') != (maze[randWall[0]][randWall[1] - 1] == 'c') and (maze[randWall[0]][randWall[1] + 1] == 'u') != (maze[randWall[0]][randWall[1] - 1] == 'u')
-        
-        if vertical or horizontal:
-          maze[randWall[0]][randWall[1]] = 'c'
-          for x in neighbors([randWall[0], randWall[1]]):
-            if maze[x[0]][x[1]] == 'u':
-              maze[x[0]][x[1]] = 'w'
-              wlist.append([x[0], x[1]])
-        elif not vertical and not horizontal:
-          maze[randWall[0]][randWall[1]] = 'w'
-    else:
-      maze[randWall[0]][randWall[1]] = 'w'
-    wlist.remove(randWall)
+    while wlist:
+        randWall = random.choice(wlist)
+        if 0 < randWall[0] < size + 1 and 0 < randWall[1] < size + 1:
+            if [maze[x[0]][x[1]] == 'c' for x in
+                neighbors([randWall[0], randWall[1]])].count(True) < 2:
+                vertical = (maze[randWall[0] + 1][randWall[1]] == 'c') != (maze[randWall[0] - 1][randWall[1]] == 'c') and (maze[randWall[0] + 1][randWall[1]] == 'u') != (maze[randWall[0] - 1][randWall[1]] == 'u')
+                horizontal = (maze[randWall[0]][randWall[1] + 1] == 'c') != (maze[randWall[0]][randWall[1] - 1] == 'c') and (maze[randWall[0]][randWall[1] + 1] == 'u') != (maze[randWall[0]][randWall[1] - 1] == 'u')
+                if vertical or horizontal:
+                    maze[randWall[0]][randWall[1]] = 'c'
+                    for x in neighbors([randWall[0], randWall[1]]):
+                        if maze[x[0]][x[1]] == 'u':
+                            maze[x[0]][x[1]] = 'w'
+                            wlist.append([x[0], x[1]])
+                elif not vertical and not horizontal:
+                    maze[randWall[0]][randWall[1]] = 'w'
+        else:
+            maze[randWall[0]][randWall[1]] = 'w'
+        wlist.remove(randWall)
 
-  #normalize cell values
-  for y,x in enumerate(maze):
-    for z in range(len(x)):
-      if x[z] != 'c':
-        maze[y][z] = 0
-      else:
-        maze[y][z] = 1
+    # normalize cell values
+    for (y, x) in enumerate(maze):
+        for z in range(len(x)):
+            if x[z] != 'c':
+                maze[y][z] = 0
+            else:
+                maze[y][z] = 1
 
-  end = random.choice([i for i in range(size + 1)[1:-1] if maze[-2][i-1] != maze[-2][i+1]])
-  return maze, [start, start], [size, end]
+    end = random.choice([i for i in range(size + 1)[1:-1] if maze[-2][i - 1] != maze[-2][i + 1]])
+    return (maze, [start, start], [size, end])
--- pathfind.py
+++ pathfind.py
@@ -5,32 +5,34 @@ from matplotlib.colors import ListedColormap
 size = 50
 
 def aStar(maze, start, end):
-  queue = [end + [0]]
-  i = 0
-  while queue:
-    current = queue[i]
-    #end found
-    if current[:-1] == start:
-      path = [current]
-      while path[-1][:-1] != end:
-        path.append(min([x for x in queue if x[:-1] in [x for x in neighbors(path[-1][:-1])]], key=lambda x: x[2]))
-      return path
-    
-    neighbor = [x + [current[2]+1] for x in neighbors(current[:-1])]
-    for x in list(neighbor):
-      if maze[x[0]][x[1]] == 0 or x[:-1] in [x[:-1] for x in queue]:
-        neighbor.remove(x)
-    queue += neighbor
-    i += 1
+    queue = [end + [0]]
+    i = 0
+    while queue:
+        current = queue[i]
+
+    # end found
+        if current[:-1] == start:
+            path = [current]
+            while (path[-1])[:-1] != end:
+                path.append(min([x for x in queue if x[:-1] in [x
+                            for x in neighbors((path[-1])[:-1])]], key=lambda x: x[2]))
+            return path
+
+        neighbor = [x + [current[2] + 1] for x in neighbors(current[:-1])]
+        for x in list(neighbor):
+            if maze[x[0]][x[1]] == 0 or x[:-1] in [x[:-1] for x in queue]:
+                neighbor.remove(x)
+        queue += neighbor
+        i += 1
 
 if __name__ == '__main__':
-  maze = generateMaze(size)
-  path = aStar(maze[0], maze[1], maze[2])
-  for x in path:
-    maze[0][x[0]][x[1]] = 2
-  maze[0][-2][maze[2][1]] = 3
-  maze[0][1][1] = 4
-
-  plt.imshow(maze[0], cmap=ListedColormap(['k', 'w', '#99d9ea', '#ed1c24', '#22b14c']))
-  plt.axis('off')
-  plt.show()
+    maze = generateMaze(size)
+    path = aStar(maze[0], maze[1], maze[2])
+    for x in path:
+        maze[0][x[0]][x[1]] = 2 
+    maze[0][-2][maze[2][1]] = 3
+    maze[0][1][1] = 4
+	
+    plt.imshow(maze[0], cmap=ListedColormap(['k', 'w', '#99d9ea', '#ed1c24', '#22b14c']))
+    plt.axis('off')
+    plt.show()
