e1da79f976066cb08e82da4c4064a61bda3b7fca
Justin Mei <imjustinmei@gmail.com>
2022-10-09 02:28:24 -0400

    clean up code, allow reanimation

--- src/js/index.js
+++ src/js/index.js
@@ -1,32 +1,29 @@
-let size = document.getElementById('size');
-let sizeValue = document.getElementById('sizeValue');
-let speed = document.getElementById('speed');
-let speedValue = document.getElementById('speedValue');
+const size = document.getElementById('size');
+const sizeValue = document.getElementById('sizeValue');
+const speed = document.getElementById('speed');
+const speedValue = document.getElementById('speedValue');
+const maze = document.getElementById('maze');
 
-size.oninput = () => {
-  sizeValue.innerHTML = size.value;
-};
+let animating = (newMaze = false);
+let clone;
 
-speed.oninput = () => {
-  speedValue.innerHTML = speed.value;
-};
+['size', 'speed'].map((e) => {
+  let input = eval(e);
+  input.oninput = () => {
+    eval(e + 'Value').innerHTML = input.value;
+  };
+});
 
-let newMaze = (generating = false);
 async function generate() {
-  newMaze = true;
-  if (generating) {
-    return;
-  }
-  generating = true;
+  if (animating) return;
+
+  animating = true;
+  maze.innerHTML = '';
+
   let currentSize = parseInt(size.value);
   let [mazeArray, startCell, endCell] = generateMaze(currentSize);
-  let [path, queue] = aStar(mazeArray, startCell, endCell);
 
-  sessionStorage.path = JSON.stringify(path);
-  sessionStorage.queue = JSON.stringify(queue);
-
-  let maze = document.getElementById('maze');
-  maze.innerHTML = '';
+  // "drawing"
   for (let i = 0; i < currentSize + 2; i++) {
     let row = document.createElement('div');
     row.className = 'row';
@@ -36,40 +33,41 @@ async function generate() {
       cell.className = mazeArray[i][j] == 1 ? '' : 'wall';
       cell.classList.add('cell');
       cell.id = String(i) + ',' + String(j);
+
+      if (cell.id == '1,1') cell.classList.add('start');
+      if (cell.id == endCell[0] + ',' + endCell[1]) cell.classList.add('end');
       row.appendChild(cell);
     }
     maze.appendChild(row);
-    let current = new Promise((resolve) => setTimeout(resolve, 4));
-    await current;
+    await new Promise((resolve) => setTimeout(resolve, 2));
   }
 
-  // color cells
-  let start = document.getElementById('1,1');
-  start.classList.add('start');
-  let end = document.getElementById(String(path[path.length - 1][0]) + ',' + String(path[path.length - 1][1]));
-  end.classList.add('end');
-  generating = false;
+  let [path, queue] = aStar(mazeArray, startCell, endCell);
+
+  sessionStorage.setItem('pathfind', JSON.stringify({ path: path, queue: queue }));
+
+  clone = maze.cloneNode(true);
+  newMaze = true;
+  animating = false;
 }
 
 async function animateMaze(demonstration) {
-  if (!newMaze) {
-    return;
-  }
-  newMaze = false;
+  if (animating) return;
+  animating = true;
+
+  if (!newMaze) maze.innerHTML = clone.innerHTML;
+
   let animationSpeed = speed.value / 2;
-  let path = JSON.parse(sessionStorage.path);
-  let queue = JSON.parse(sessionStorage.queue);
+  let { path, queue } = JSON.parse(sessionStorage.getItem('pathfind'));
 
   // demonstration
   if (demonstration) {
     for (let c of queue.slice(1, queue.length)) {
-      if (String(c[0]) + ',' + String(c[1]) == '1,1') {
-        break;
-      }
+      if (String(c[0]) + ',' + String(c[1]) == '1,1') break;
+
       let cell = document.getElementById(String(c[0]) + ',' + String(c[1]));
       cell.style.backgroundColor = 'hsl(12, 65%, ' + (95 - (20 * c[2]) / path.length) + '%)';
-      let current = new Promise((resolve) => setTimeout(resolve, 5 / animationSpeed));
-      await current;
+      await new Promise((resolve) => setTimeout(resolve, 5 / animationSpeed));
     }
   }
 
@@ -77,7 +75,8 @@ async function animateMaze(demonstration) {
   for (let c of path.slice(1, path.length - 1)) {
     let pathCell = document.getElementById(String(c[0]) + ',' + String(c[1]));
     pathCell.style.backgroundColor = 'hsl(193, 66%, ' + (76 + (19 * c[2]) / path.length) + '%)';
-    let current = new Promise((resolve) => setTimeout(resolve, 75 - animationSpeed));
-    await current;
+    await new Promise((resolve) => setTimeout(resolve, 75 - animationSpeed));
   }
+
+  animating = newMaze = false;
 }
