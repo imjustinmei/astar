88936540730fb75db9254fdd1d83faab6653ac83
Justin Mei <imjustinmei@gmail.com>
2024-11-17 17:04:47 -0500

    limit sizing

--- src/js/index.js
+++ src/js/index.js
@@ -1,50 +1,54 @@
-const size = document.getElementById('size');
-const sizeValue = document.getElementById('sizeValue');
-const speed = document.getElementById('speed');
-const speedValue = document.getElementById('speedValue');
-const maze = document.getElementById('maze');
+const size = document.getElementById("size");
+const sizeValue = document.getElementById("sizeValue");
+const speed = document.getElementById("speed");
+const speedValue = document.getElementById("speedValue");
+const maze = document.getElementById("maze");
 
-let animating = newMaze = false;
+let animating = (newMaze = false);
 let clone;
 
-['size', 'speed'].map((e) => {
+["size", "speed"].map((e) => {
   let input = eval(e);
   input.oninput = () => {
-    eval(e + 'Value').innerHTML = input.value;
+    if (e == "size") input.value = input.value - (input.value % 2 == 0 ? 1 : 0);
+    eval(e + "Value").innerHTML = input.value;
   };
 });
 
 async function generate() {
   if (animating) return;
   animating = true;
-  maze.innerHTML = '';
+  maze.innerHTML = "";
 
   let currentSize = parseInt(size.value);
   let [mazeArray, startCell, endCell] = generateMaze(currentSize);
 
   // "drawing"
   for (let i = 0; i < currentSize + 2; i++) {
-    let row = document.createElement('div');
-    row.className = 'row';
+    let row = document.createElement("div");
+    row.className = "row";
 
     for (let j = 0; j < currentSize + 2; j++) {
-      let cell = document.createElement('div');
+      let cell = document.createElement("div");
       cell.className = "cell";
-      if (mazeArray[i][j] != 1) cell.classList.add('wall');
-      cell.id = String(i) + ',' + String(j);
+      if (mazeArray[i][j] != 1) cell.classList.add("wall");
+      cell.id = String(i) + "," + String(j);
 
       row.appendChild(cell);
     }
     maze.appendChild(row);
     if (i % 4 == 0) await new Promise((resolve) => setTimeout(resolve, 1));
   }
-  document.getElementById('1,1').classList.add('start')
-  document.getElementById(endCell[0] + ',' + endCell[1]).classList.add('end')
+  document.getElementById("1,1").classList.add("start");
+  document.getElementById(endCell[0] + "," + endCell[1]).classList.add("end");
 
   setTimeout(() => {
     let [path, queue] = aStar(mazeArray, startCell, endCell);
 
-    sessionStorage.setItem('pathfind', JSON.stringify({ path: path, queue: queue }));
+    sessionStorage.setItem(
+      "pathfind",
+      JSON.stringify({ path: path, queue: queue })
+    );
 
     newMaze = true;
     clone = maze.cloneNode(true);
@@ -58,25 +62,28 @@ async function animateMaze(demonstration) {
 
   if (!newMaze) maze.innerHTML = clone.innerHTML;
   let animationSpeed = speed.value;
-  let { path, queue } = JSON.parse(sessionStorage.getItem('pathfind'));
+  let { path, queue } = JSON.parse(sessionStorage.getItem("pathfind"));
 
   // demonstration
   if (demonstration) {
     let count = 0;
     for (let c of queue.slice(1, queue.length)) {
-      if (String(c[0]) + ',' + String(c[1]) == '1,1') break;
+      if (String(c[0]) + "," + String(c[1]) == "1,1") break;
       count++;
 
-      let cell = document.getElementById(String(c[0]) + ',' + String(c[1]));
-      cell.style.backgroundColor = 'hsl(12, 65%, ' + (95 - (20 * c[2]) / path.length) + '%)';
-      if (count % animationSpeed == 0) await new Promise((resolve) => setTimeout(resolve, 9 - animationSpeed));
+      let cell = document.getElementById(String(c[0]) + "," + String(c[1]));
+      cell.style.backgroundColor =
+        "hsl(12, 65%, " + (95 - (20 * c[2]) / path.length) + "%)";
+      if (count % animationSpeed == 0)
+        await new Promise((resolve) => setTimeout(resolve, 9 - animationSpeed));
     }
   }
 
   // final path
   for (let c of path.slice(1, path.length - 1)) {
-    let pathCell = document.getElementById(String(c[0]) + ',' + String(c[1]));
-    pathCell.style.backgroundColor = 'hsl(193, 66%, ' + (76 + (19 * c[2]) / path.length) + '%)';
+    let pathCell = document.getElementById(String(c[0]) + "," + String(c[1]));
+    pathCell.style.backgroundColor =
+      "hsl(193, 66%, " + (76 + (19 * c[2]) / path.length) + "%)";
     await new Promise((resolve) => setTimeout(resolve, 15));
   }
 
--- src/js/maze.js
+++ src/js/maze.js
@@ -8,42 +8,68 @@ function neighborCells([x, y], size) {
 }
 
 function generateMaze(size) {
-  let maze = Array.from({ length: size + 2 }, () => new Array(size + 2).fill(''));
-  let wallList = neighborCells([1, 1], size);
-  for (let wall of wallList) maze[wall[0]][wall[1]] = 's';
-  maze[1][1] = 'c';
+  let start = [1, 1];
+  let maze = Array.from({ length: size + 2 }, () =>
+    new Array(size + 2).fill("")
+  );
+  let wallList = neighborCells(start, size);
+  for (let wall of wallList) maze[wall[0]][wall[1]] = "s";
+  maze[1][1] = "c";
 
   while (wallList.length) {
     let tentative = wallList[Math.floor(Math.random() * wallList.length)];
     let notEdge = tentative[0] != size + 1 && tentative[1] != size + 1;
     let nearbyCells = neighborCells(tentative, size).filter((cell) => {
       value = maze[cell[0]][cell[1]];
-      if (value == 'c') return cell;
+      if (value == "c") return cell;
       if (!value) {
-        maze[cell[0]][cell[1]] = 's';
+        maze[cell[0]][cell[1]] = "s";
         if (notEdge) wallList.push(cell);
       }
     });
 
-    let chosenCell = nearbyCells[Math.floor(Math.random() * nearbyCells.length)];
-    maze[(tentative[0] + chosenCell[0]) / 2][(tentative[1] + chosenCell[1]) / 2] = 'c';
-    if (notEdge) maze[tentative[0]][tentative[1]] = 'c';
+    let chosenCell =
+      nearbyCells[Math.floor(Math.random() * nearbyCells.length)];
+    maze[(tentative[0] + chosenCell[0]) / 2][
+      (tentative[1] + chosenCell[1]) / 2
+    ] = "c";
+    if (notEdge) maze[tentative[0]][tentative[1]] = "c";
 
     wallList.splice(wallList.indexOf(tentative), 1);
   }
 
   // normalize values
   for (let i = 1; i < size + 1; i++) {
-    if (maze[size + 1][i] == 's') maze[size][i] = 'c';
-    if (maze[i][size + 1] == 's') maze[i][size] = 'c';
+    if (maze[size + 1][i] == "s") maze[size][i] = "c";
+    if (maze[i][size + 1] == "s") maze[i][size] = "c";
   }
 
   for (let i = 0; i < size + 2; i++) {
     for (let j = 0; j < size + 2; j++) {
-      if (maze[i][j] != 'c') maze[i][j] = 0;
+      if (maze[i][j] != "c") maze[i][j] = 0;
       else maze[i][j] = 1;
     }
   }
-  let end = maze[size].reduce((r, d, i) => (d == 1 ? (r.push(i), r) : r), []);
-  return [maze, [1, 1], [size, end[Math.floor(Math.random() * end.length)]]];
+
+  let deadEnds = [];
+  let verticals = 0;
+
+  for (let i = 1; i < size; i++) {
+    if (neighbors([size, i]).reduce((r, d) => r + maze[d[0]][d[1]], 0) == 1) {
+      deadEnds.unshift(i);
+      verticals++;
+    }
+    if (neighbors([i, size]).reduce((r, d) => r + maze[d[0]][d[1]], 0) == 1)
+      deadEnds.push(i);
+  }
+
+  let endIndex = Math.floor(Math.random() * deadEnds.length);
+
+  return [
+    maze,
+    start,
+    endIndex < verticals
+      ? [size, deadEnds[endIndex]]
+      : [deadEnds[endIndex], size],
+  ];
 }
